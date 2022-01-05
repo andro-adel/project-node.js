c13c559da6457d20340054ac35e030445df25dcd
Andro <androadel699@gmail.com>
2022-01-05 08:49:56 +0200

    edit in app

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Andro Adel Yousef')
+app.get('/', (req, res) =>
+{
+  res.redirect("/home")
+
+  res.send('<h1>Index Page</h1>')
+})
+
+app.get('/home', (req, res) => {
+  res.send('<h1>Home Page</h1>')
 })
 
 app.listen(port, () => {
