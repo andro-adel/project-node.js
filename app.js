9837e58b555c10017253fe5dd24c77e6b7bbeca0
Andro <androadel699@gmail.com>
2022-01-05 08:50:27 +0200

    edit

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) =>
-{
-  res.redirect("/home")
+app.get("/", (req, res) => {
+  res.redirect("/home");
 
-  res.send('<h1>Index Page</h1>')
-})
+  res.send("<h1>Index Page</h1>");
+});
 
-app.get('/home', (req, res) => {
-  res.send('<h1>Home Page</h1>')
-})
+app.get("/home", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
