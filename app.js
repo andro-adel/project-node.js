2b35d6ceaffdce80850518e56fe6551c89cb91fa
Andro <androadel699@gmail.com>
2022-02-02 11:49:41 +0200

    edit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect("/home");
@@ -10,6 +12,19 @@ app.get("/", (req, res) => {
 
 app.get("/home", (req, res) => {
   res.send("<h1>Home Page</h1>");
+});
+
+app.get("/html", (req, res) => {
+  res.sendFile("views/index.ejs", { root: __dirname });
+});
+
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+
+// 404
+app.use((req, res) => {
+  res.status(404).send("Sorry can't find that!");
 });
 
 app.listen(port, () => {
